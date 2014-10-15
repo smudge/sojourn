4b0064b6e12a07f373bec6397b2e2841e5ef97c3
Smudge <nathan@ngriffith.com>
2014-10-15 18:41:13 -0400

    If current_user changes, ensure current visitor updates accordingly.

--- lib/sojourn/controller.rb
+++ lib/sojourn/controller.rb
@@ -2,11 +2,21 @@ module Sojourn
   module Controller
 
     def self.included(base)
+      base.before_filter :check_sojourn_user
       base.before_filter :track_sojourn_visit
     end
 
     def track_sojourn_visit
       Visit.create_from_request!(request, current_visitor) if current_visitor.visits.empty?
+    end
+
+    def check_sojourn_user
+      return if current_visitor.user_id == current_user.try(:id)
+      if current_visitor.user_id.nil?
+        current_visitor.update_attribute(:user, current_user)
+      else
+        clear_current_visitor
+      end
     end
 
     def current_visitor
@@ -17,6 +27,11 @@ module Sojourn
         @current_visitor ||= Visitor.find_by_uuid!(session[:visitor_uuid])
       end
       @current_visitor
+    end
+
+    def clear_current_visitor
+      @current_visitor = nil
+      session[:visitor_uuid] = nil
     end
 
   end
