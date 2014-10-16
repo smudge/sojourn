49402b8d75422c21a9756ac75fea05d6f5c2721b
Smudge <nathan@ngriffith.com>
2014-10-16 14:28:01 -0400

    Refactoring visitor-finding complexity from controller into model.

--- lib/sojourn/controller.rb
+++ lib/sojourn/controller.rb
@@ -2,7 +2,6 @@ module Sojourn
   module Controller
 
     def self.included(base)
-      base.before_filter :check_sojourn_user
       base.before_filter :current_visit
     end
 
@@ -11,29 +10,9 @@ module Sojourn
       @current_visit ||= Visit.create_from_request!(request, current_visitor)
     end
 
-    def check_sojourn_user
-      return if current_visitor.user_id == current_user.try(:id)
-      if current_visitor.user_id.nil?
-        current_visitor.update_attribute(:user, current_user)
-      else
-        clear_current_visitor
-      end
-    end
-
     def current_visitor
-      if session[:visitor_uuid]
-        @current_visitor = Visitor.where(uuid: session[:visitor_uuid]).unexpired.last
-      end
-      unless @current_visitor
-        @current_visitor = Visitor.create_from_request!(request, current_user)
-        session[:visitor_uuid] = @current_visitor.uuid
-      end
-      @current_visitor
-    end
-
-    def clear_current_visitor
-      @current_visitor = nil
-      session[:visitor_uuid] = nil
+      @current_visitor ||= Visitor.find_from_session(session, current_user)
+      @current_visitor ||= Visitor.create_from_request!(request, session, current_user)
     end
 
   end
--- lib/sojourn/visitor.rb
+++ lib/sojourn/visitor.rb
@@ -13,8 +13,26 @@ module Sojourn
 
     before_create { self.uuid = SecureRandom.uuid }
 
-    def self.create_from_request!(request, user = nil)
-      create! ip_address: request.remote_ip, user_agent: request.user_agent, user: user
+    class << self
+
+      def find_from_session(session, user = nil)
+        with_user where(uuid: session[:visitor_uuid]).unexpired.last, session, user
+      end
+
+      def create_from_request!(request, session, user = nil)
+        visitor = create! ip_address: request.remote_ip, user_agent: request.user_agent, user: user
+        session[:visitor_uuid] = visitor.uuid
+        visitor
+      end
+
+    private
+
+      def with_user(visitor, session, user)
+        return visitor if visitor.nil? || visitor.user_id == user.try(:id)
+        visitor.update_attributes(user: user) and return visitor if user && visitor.user_id.nil?
+        session[:visitor_uuid] = nil
+      end
+
     end
 
   end
