c1ca477188fc774add9f81660da2867d222b616b
Smudge <nathan@ngriffith.com>
2014-11-21 15:27:27 -0500

    Removing unused configuration attributes.

--- lib/sojourn/configuration.rb
+++ lib/sojourn/configuration.rb
@@ -1,8 +1,6 @@
 class Configuration
 
   attr_accessor :campaign_params
-  attr_accessor :visitor_expires_after
-  attr_accessor :visit_expires_after
 
   def initialize
     set_defaults
@@ -12,8 +10,6 @@ private
 
   def set_defaults
     self.campaign_params = [:utm_source, :utm_medium, :utm_term, :utm_content, :utm_campaign]
-    self.visitor_expires_after = 1.week
-    self.visit_expires_after = 1.day
   end
 
 end
