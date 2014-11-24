56bc7c95c988ce271d784865bacaf76ef0bc676a
Smudge <nathan@ngriffith.com>
2014-11-24 15:32:00 -0500

    New feature for events: Will automatically store event properties in column of the same name, if it exists, for easier queryability of common properties.

--- lib/sojourn/event.rb
+++ lib/sojourn/event.rb
@@ -1,7 +1,19 @@
 module Sojourn
   class Event < ActiveRecord::Base
+    DEFAULT_FIELDS = [:id, :sojourner_uuid, :name, :properties, :sojourn_request_id, :user_id, :created_at]
+
     belongs_to :request, foreign_key: :sojourn_request_id
     belongs_to :user
     serialize :properties
+
+    before_save do
+      properties.keys.map(&:to_sym).each do |key|
+        send("#{key}=", properties[key]) if self.class.available_fields.include?(key)
+      end
+    end
+
+    def self.available_fields
+      @available_fields ||= column_names.map(&:to_sym) - DEFAULT_FIELDS
+    end
   end
 end
