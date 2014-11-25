6c3deca06d2570fabeaf420fe0d55e55e8580943
Smudge <nathan@ngriffith.com>
2014-11-25 15:58:46 -0500

    Add index to sojourn_events.sojourner_uuid

--- lib/generators/sojourn/templates/create_sojourn_events.rb
+++ lib/generators/sojourn/templates/create_sojourn_events.rb
@@ -8,6 +8,7 @@ class CreateSojournEvents < ActiveRecord::Migration
       t.references :user
       t.timestamp :created_at
     end
+    add_index :sojourn_events, [:sojourner_uuid]
     add_index :sojourn_events, [:sojourn_request_id]
     add_index :sojourn_events, [:user_id]
     add_index :sojourn_events, [:name]
